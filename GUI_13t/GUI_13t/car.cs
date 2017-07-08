ee94c3e5d1664d56e430267a955a01d43a202826
ThanhPT <ThanhPT@THANHPT-ILANDAP>
2017-07-08 16:42:51 +0700

    Add project files.

---
+++ GUI_13t/GUI_13t/car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_13t
+{
+    public class car
+    {
+        public string CarName { get; set; } 
+    }
+}
